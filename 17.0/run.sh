0f7f1df054e82ba24ec4ad9109f9c44339eb9fe4
maso <mostafa.barmshory@gmail.com>
2025-08-19 01:17:57 +0330

    [CP] Merge with branch 17.0

---
+++ 17.0/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+fileWorkspaceFolder="/home/maso/Project/Odoo-16/docker"
+docker run --rm \
+    --network psql_default \
+    --volume ${fileWorkspaceFolder}/../odoo:/var/lib/odoo \
+    --volume ${fileWorkspaceFolder}/../:/extra-add \
+    -p 8069:8069 \
+    9a75d868d261 \
+    --db_host db \
+    --db_password odoo \
+    --db_user odoo \
+    --database odoo16-main \
+    --addons-path "/extra-add/odoo/addons,/extra-add/CybroAddons,/extra-add/odoo-exchange,/extra-add/odoo-viraweb123,/extra-add/odoo-mobile-service,/extra-add/sale-workflow,/extra-add/iot,/extra-add/odoo-beton,/extra-add/trip2persia,/extra-add/account-reconcile,/extra-add/bank-statement-import,/extra-add/odoo-hr" \
+    --load vw_odooo_patch \
+    --update  vw_hr_attendence \
+    --dev all 
+
